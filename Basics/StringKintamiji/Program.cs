6de8d19bb4e34e20eb4e91bc1cee15ccbb3ade26
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-06-07 20:49:41 +0300

    test

--- Basics/StringKintamiji/Program.cs
+++ Basics/StringKintamiji/Program.cs
@@ -6,8 +6,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string hello = "Hello World!";
-            Console.WriteLine(hello);
+            string hello = "Hello";
+            string world = " World!";
+
+            string tuscias = "";
+            string nulas = null;
+            string laisvaerdve = "";
+
+            //Galima naudoti tuos pacius tris su visais string kintamaisiai
+
+            string konkatinacija = hello + world;
+            Console.WriteLine(konkatinacija);
+
+            string kompozicija = String.Format("{0}{1}", hello, world);
+            Console.WriteLine(kompozicija);
+
+            string interpoliacija = $"{hello}{world}";
+            Console.WriteLine(interpoliacija);
+            
+
         }
 
     }
