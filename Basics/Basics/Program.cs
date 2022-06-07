b6a660db54ffd1490be073385d23986f5c0b3ab0
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-06-07 19:12:36 +0300

    i

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -17,8 +17,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             Console.WriteLine("--------------------------------------------------");
 
-            Console.WriteLine("isvedimas " + "vienoje " + "eiluteje"); // konkatinacija
-            Console.WriteLine();
+            Console.WriteLine("isvedimas " + "vienoje " + "eiluteje"); // konkatinacija, naudoja daug atminties
+            Console.WriteLine("{0} {1} {2}", "isvedimas", "vienoje", "eiluteje"); // kompozicija
+            Console.WriteLine($"{"isvedimas"} vienoje {"eiluteje"}"); // interpoliacija, skaitomiausias, kencia performance
+
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("tekstas idedamas i \"kabutes\""); // \ yra escape simbolis
+            Console.WriteLine("tekstas bus perkeltas i nauja eilute \nDABAR");
+            Console.WriteLine("dideli \ttarpai \ttarp \tzodziu");
         }
+
     }
 }
---
+++ Basics/Console uzduotys/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // isveskite i ekrana savo varda
+            //Console.WriteLine("Domas");
+
+            //nuskaitykite is klaviaturos savo varda ir isveskite i ekrana
+            //Console.WriteLine("     "  +  Console.ReadLine());
+
+            //nuskaitykite is ekrano savo vardo pirma raide ir isveskite i ekrana jos ASCII koda
+            //Console.WriteLine(" raides ASCII   " + (int)Console.ReadKey().KeyChar);
+
+            //Nuskaitykite raide, nuskaitykite skaiciu, isveskite suma
+            //Reikia atimti 48, nes kai paspaudi pvz 1, jis koduojamas skaiciumi 49. Kad vel turetume 1, turim atimti 48 
+            //Console.WriteLine("Iveskite raide ir bet koki skaiciu");
+            //Console.WriteLine("     rezultatas yra: {0}", (int)Console.ReadKey().KeyChar + (int)Console.ReadKey().KeyChar - 48);// <---- atimama 48, kad is ASCII simboliu kodo gautume skaiciu
+
+            //isvesti i ekrana pasirinka punkta
+            /*Console.WriteLine("(1) Pirkti");
+            Console.WriteLine("(2) Parduoti");
+            Console.WriteLine("(3) Likuciai");
+            Console.WriteLine("Pasirinkimas yra: \"{0}\"", Console.ReadKey().KeyChar);
+            Console.WriteLine($"Pasirinkimas yra: \"{Console.ReadKey().KeyChar}\"");
+            Console.WriteLine("Pasirinkimas yra: \"" + Console.ReadKey().KeyChar + "\"");
+            */
+
+            //Zodzio LABAS kiekviena raide kitoj eilutej
+            /*Console.WriteLine("L");
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+            Console.WriteLine("A");
+            Console.WriteLine("S");
+            //ARBA
+            Console.WriteLine("L\nA\nB\nA\nS");
+            */
+
+            //LABAS su dideliais tarpain
+                //Console.WriteLine("L\tA\tB\tA\tS");
+        }
+
+    }
+}
