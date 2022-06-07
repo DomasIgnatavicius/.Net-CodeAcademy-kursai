251580f601ad67eba00aeba2ce806c6133f37e8e
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-06-07 16:20:12 +0300

    110

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("isvedimas");
+            Console.WriteLine("vienoje");
+            Console.WriteLine("eiluteje");
+            Console.WriteLine();
+            Console.WriteLine("Tekstas kitoje eiluteje");
+            Console.Write("tekstas");
+
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.WriteLine("isvedimas " + "vienoje " + "eiluteje"); // konkatinacija
+            Console.WriteLine();
+        }
+    }
+}
