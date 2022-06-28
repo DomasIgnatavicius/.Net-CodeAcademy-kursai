a8e664206cb0d8b00938f9cc5da55bc133a0e459
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-06-28 14:25:30 +0300

    iii

---
+++ Basics/P009 String Manipulation/Program.cs
@@ -0,0 +1,22 @@
+﻿
+stringManipulationTeorija();
+
+
+void stringManipulationTeorija()
+{
+    Console.WriteLine("Hello string manipulation");
+    Console.WriteLine();
+
+    Console.WriteLine("*********String constructor**********");
+
+    char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+    string greetings = new string(letters); // string constructor, sudeda viena string is masyvo
+    Console.WriteLine(greetings);
+
+    Console.WriteLine(letters[2]);
+    Console.WriteLine(greetings[2]);
+
+    //-------------------------------------------------------
+
+    Console.WriteLine("*********String concatination**********");
+}
