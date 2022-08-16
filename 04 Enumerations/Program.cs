3d4a934a50b19737b4c5aad4e4b40d1cc0ad825e
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-08-16 19:21:34 +0300

    best enum variantas

--- 04 Enumerations/Program.cs
+++ 04 Enumerations/Program.cs
@@ -28,7 +28,7 @@
             //statines klases enumeracijos - siek tiek geriau, nes programa tampa labiau modifikuojama ir gali plestis
 
             int today3 = DayOfWeekEnum.Tuesday; // toks pat panaudojimas kaip enum, tik cia yra static klase - atsiranda galimybe siek tiek praplesti funkcionaluma, o sintakse visiskai nesikeicia
-            //testinu repo
+            
         
         }
     }
@@ -45,4 +45,28 @@ public static class DayOfWeekEnum
     public static int Thursday = 5;
     public static int Friday = 6;
     public static int Saturday = 7;
+}
+
+//--------------------------GERIAUSIAS VARIANTAS-------------------------------
+public class CustomEnum
+{
+    public CustomEnum(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    public int id { get; set; }
+    public string name { get; set; }
+    public override string ToString() => name;
+}
+
+public class DayOfWeekCustomEnum
+{
+    public static CustomEnum Sunday => new CustomEnum(1, nameof(Sunday));
+    public static CustomEnum Monay => new CustomEnum(1, nameof(Monay));
+    public static CustomEnum Tuesday => new CustomEnum(1, nameof(Tuesday));
+    public static CustomEnum Wednesday => new CustomEnum(1, nameof(Wednesday));
+    public static CustomEnum Thursday => new CustomEnum(1, nameof(Thursday));
+    public static CustomEnum Friday => new CustomEnum(1, nameof(Friday));
+    public static CustomEnum Saturday => new CustomEnum(1, nameof(Saturday));
 }
