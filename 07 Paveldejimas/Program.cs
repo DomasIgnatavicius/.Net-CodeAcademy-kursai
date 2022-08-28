ee69664e59dd5cf4acceb5651aafabb19f60e04a
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-08-28 20:42:56 +0300

    Paveldejimo teorija

--- 07 Paveldejimas/Program.cs
+++ 07 Paveldejimas/Program.cs
@@ -4,9 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var square1 = new Square();
+            Console.WriteLine($"Priskiriam keturkampiui keturis kampus: {square1.NumberOfAngles = 4}");
+
+            var triangle1 = new Triangle();
+            Console.WriteLine($"Priskiriam trikampiui tris kampus: {triangle1.NumberOfAngles}");
+
+            Polygon square2 = new Square(); // visiskai veikia toks priskyrimas, nes square yra paveldetas is polygon. sitoje klaseje negalesime pasiekti vaikiniu prop, nes tipas yra tevinis
+            //square2.AnglesInDegrees = 90 NEVEIKIA NES TIPAS YRA POLYGON O NE SQUARE, TEVAI NEPASIEKIA VAIKINIO SCOUPO
+            ((Square)square2).AngleInDegrees = 90; // taip veikia, nes pacastinam i vaikine klase
+
+            Console.WriteLine("----------------------------------");
+
+            List<Polygon> polygons = new List<Polygon>();
+            polygons.Add(square);
+            polygons.Add(triangle);
+            polygons.Add(square2);
+            foreach (var item in polygons)
+            {
+                Console.WriteLine($" {item.GetType().Name} NumberOfAngles = " + item.NumberOfAngles);
+                if (item is Square)
+                {
+                    Console.WriteLine("Size in item =" + ((Square)item).Size);
+                }
+                Console.WriteLine("GetPerimeter() in item =" + item.GetPerimeter());
+
+            }
         }
 
+        // Base class / Parent class - tai klase kuri vienija
         public class Polygon
         {
             public Polygon()
@@ -20,6 +46,43 @@
             }
 
             public int NumberOfAngles { get; set; }
+
+
+            public virtual double GetPerimeter() //virtual pasako kad paveldetoje klaseje galima metoda perrašyti
+            {
+                return 0;
+            }
         }
+
+        public class Square : Polygon 
+        {
+            public Square()
+            {
+                NumberOfAngles = 4; // del paveldejimo numberOfAngles yra pasiekiamas Square klaseje
+            }
+
+            public Square(double size)
+            {
+                Size = size;
+                NumberOfAngles = 4;
+            }
+            public int AngleInDegrees { get; set; }
+            public double Size { get; set; }
+        }
+
+        public class Triangle : Polygon
+        {
+            public Triangle()
+            {
+                NumberOfAngles = 3;
+            }
+
+            public override double GetPerimeter() // overridinam tevines klases metoda
+            {
+                return NumberOfAngles + 10;
+            }
+        }
+
+
     }
 }
