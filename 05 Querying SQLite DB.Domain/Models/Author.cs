16ba1f8b81c714558fe634b38863a0152735b7b7
domasIgnatavicius <ignatavicius.domas@gmail.com>
2022-09-19 16:14:46 +0300

    Query SQLite added initial models

---
+++ 05 Querying SQLite DB.Domain/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Querying_SQLite_DB.Domain.Models
+{
+    internal class Author
+    {
+    }
+}
---
+++ 05 Querying SQLite DB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_Querying_SQLite_DB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
